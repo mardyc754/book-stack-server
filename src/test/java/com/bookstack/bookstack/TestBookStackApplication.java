4f232cc83f6323c9288d9206ee70ac0d7318fe87
Marta Dychała <mar754dyc@gmail.com>
2024-04-07 18:56:35 +0200

    Init

---
+++ src/test/java/com/bookstack/bookstack/TestBookStackApplication.java
@@ -0,0 +1,23 @@
+package com.bookstack.bookstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestBookStackApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(BookStackApplication::main).with(TestBookStackApplication.class).run(args);
+    }
+
+}
